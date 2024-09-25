6e3336126266fd705bcabfce20f6caa27cb5231b
varc00l <ivalchuk4@gmail.com>
2024-09-25 13:40:45 +0300

    aded

--- script.js
+++ script.js
@@ -1,29 +1,24 @@
-// lazyLoad.js
 document.addEventListener('DOMContentLoaded', function () {
     const images = document.querySelectorAll('img[data-src]');
   
-    // Callback функція для IntersectionObserver
     const lazyLoad = (entries, observer) => {
       entries.forEach((entry) => {
-        // Якщо зображення стає видимим
         if (entry.isIntersecting) {
           const img = entry.target;
-          img.src = img.dataset.src; // Присвоєння атрибута data-src до src
+          img.src = img.dataset.src;
           img.onload = () => {
-            img.classList.add('loaded'); // Додаємо клас для анімації
+            img.classList.add('loaded'); 
           };
-          observer.unobserve(img); // Припиняємо спостереження за цим зображенням
+          observer.unobserve(img); 
         }
       });
     };
   
-    // Створюємо новий IntersectionObserver
     const observer = new IntersectionObserver(lazyLoad, {
-      root: null, // Спостерігаємо за всім viewport'ом
-      threshold: 0.1, // Зображення стає видимим, якщо 10% його видимі
+      root: null, 
+      threshold: 0.1, 
     });
   
-    // Спостерігаємо за кожним зображенням
     images.forEach((img) => {
       observer.observe(img);
     });
